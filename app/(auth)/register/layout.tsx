0312a864895962f5ae796e2c746b6533e8d63222
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-09-13 00:50:04 +0000

    layout change for prod

---
+++ app/(auth)/register/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from "@/components/SideNav";
+
+export default function AuthRegisterLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className=" h-screen relative overflow-hidden bg-[#000000a6] ">
+      {children}
+    </div>
+  );
+}
