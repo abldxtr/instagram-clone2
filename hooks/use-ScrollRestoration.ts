cda00b600d65676f9ddd22777626700e4e5fe672
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-10-26 17:58:08 +0000

    bug fix cache

--- hooks/use-ScrollRestoration.ts
+++ hooks/use-ScrollRestoration.ts
@@ -1,67 +1,67 @@
-"use client";
+// "use client";
 
-import { useEffect, useLayoutEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+// import { useEffect, useLayoutEffect } from "react";
+// import { usePathname, useSearchParams } from "next/navigation";
 
-export default function useScrollRestoration() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+// export default function useScrollRestoration() {
+//   const pathname = usePathname();
+//   const searchParams = useSearchParams();
 
-  const url = `${pathname}?${searchParams}`;
+//   const url = `${pathname}?${searchParams}`;
 
-  useLayoutEffect(() => {
-    if (typeof window !== "undefined") {
-      const beforeUnloadHandler = () => sessionStorage.clear();
+//   useLayoutEffect(() => {
+//     if (typeof window !== "undefined") {
+//       const beforeUnloadHandler = () => sessionStorage.clear();
 
-      window.addEventListener("beforeunload", beforeUnloadHandler);
+//       window.addEventListener("beforeunload", beforeUnloadHandler);
 
-      if ("history" in window && "scrollRestoration" in history) {
-        history.scrollRestoration = "manual";
-      }
+//       if ("history" in window && "scrollRestoration" in history) {
+//         history.scrollRestoration = "manual";
+//       }
 
-      const scrollY = sessionStorage.getItem(url);
+//       const scrollY = sessionStorage.getItem(url);
 
-      window.scrollTo(0, +scrollY!);
+//       window.scrollTo(0, +scrollY!);
 
-      return () => {
-        window.removeEventListener("beforeunload", beforeUnloadHandler);
-      };
-    }
-  }, [url]);
+//       return () => {
+//         window.removeEventListener("beforeunload", beforeUnloadHandler);
+//       };
+//     }
+//   }, [url]);
 
-  useEffect(() => {
-    let scrollTimeout;
-    let lastScrollTop = window.scrollY;
+//   useEffect(() => {
+//     let scrollTimeout;
+//     let lastScrollTop = window.scrollY;
 
-    const scrollHandler = () => {
-      clearTimeout(scrollTimeout);
-      scrollTimeout = setTimeout(() => {
-        if (window.scrollY !== lastScrollTop) {
-          lastScrollTop = window.scrollY;
-          sessionStorage.setItem(url, window.scrollY);
-        }
-      }, 0);
-    };
+//     const scrollHandler = () => {
+//       clearTimeout(scrollTimeout);
+//       scrollTimeout = setTimeout(() => {
+//         if (window.scrollY !== lastScrollTop) {
+//           lastScrollTop = window.scrollY;
+//           sessionStorage.setItem(url, window.scrollY);
+//         }
+//       }, 0);
+//     };
 
-    const scrollEndHandler = () => {
-      sessionStorage.setItem(url, window.scrollY);
-    };
+//     const scrollEndHandler = () => {
+//       sessionStorage.setItem(url, window.scrollY);
+//     };
 
-    if ("onscrollend" in window) {
-      // Browser supports scrollend
-      document.addEventListener("scrollend", scrollEndHandler);
-    } else {
-      // Polyfill for browsers that don't support scrollend
-      document.addEventListener("scroll", scrollHandler);
-    }
+//     if ("onscrollend" in window) {
+//       // Browser supports scrollend
+//       document.addEventListener("scrollend", scrollEndHandler);
+//     } else {
+//       // Polyfill for browsers that don't support scrollend
+//       document.addEventListener("scroll", scrollHandler);
+//     }
 
-    return () => {
-      if ("onscrollend" in window) {
-        document.removeEventListener("scrollend", scrollEndHandler);
-      } else {
-        document.removeEventListener("scroll", scrollHandler);
-        clearTimeout(scrollTimeout);
-      }
-    };
-  }, [url]);
-}
+//     return () => {
+//       if ("onscrollend" in window) {
+//         document.removeEventListener("scrollend", scrollEndHandler);
+//       } else {
+//         document.removeEventListener("scroll", scrollHandler);
+//         clearTimeout(scrollTimeout);
+//       }
+//     };
+//   }, [url]);
+// }
